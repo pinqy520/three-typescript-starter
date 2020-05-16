bf64f1e06326088af04a6f125b71820527ff49b5
Huang Qi <huangqi.0@bytedance.com>
2020-05-16 14:37:16 +0800

    feat: update dep and remove despreated lib

--- src/index.ts
+++ src/index.ts
@@ -1,69 +1,69 @@
 // add styles
-import './style.css'
+import './style.css';
 // three.js
-import * as THREE from 'three'
+import * as THREE from 'three';
 
 // create the scene
-let scene = new THREE.Scene()
+const scene = new THREE.Scene();
 
 // create the camera
-let camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000)
+const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
 
-let renderer = new THREE.WebGLRenderer()
+const renderer = new THREE.WebGLRenderer();
 
 // set size
-renderer.setSize(window.innerWidth, window.innerHeight)
+renderer.setSize(window.innerWidth, window.innerHeight);
 
 // add canvas to dom
-document.body.appendChild(renderer.domElement)
+document.body.appendChild(renderer.domElement);
 
 // add axis to the scene
-let axis = new THREE.AxesHelper(10)
+const axis = new THREE.AxesHelper(10);
 
-scene.add(axis)
+scene.add(axis);
 
 // add lights
-let light = new THREE.DirectionalLight(0xffffff, 1.0)
+const light = new THREE.DirectionalLight(0xffffff, 1.0);
 
-light.position.set(100, 100, 100)
+light.position.set(100, 100, 100);
 
-scene.add(light)
+scene.add(light);
 
-let light2 = new THREE.DirectionalLight(0xffffff, 1.0)
+const light2 = new THREE.DirectionalLight(0xffffff, 1.0);
 
-light2.position.set(-100, 100, -100)
+light2.position.set(-100, 100, -100);
 
-scene.add(light2)
+scene.add(light2);
 
-let material = new THREE.MeshBasicMaterial({
-	color: 0xaaaaaa,
-	wireframe: true
-})
+const material = new THREE.MeshBasicMaterial({
+  color: 0xaaaaaa,
+  wireframe: true,
+});
 
 // create a box and add it to the scene
-let box = new THREE.Mesh(new THREE.BoxGeometry(1, 1, 1), material)
+const box = new THREE.Mesh(new THREE.BoxGeometry(1, 1, 1), material);
 
-scene.add(box)
+scene.add(box);
 
-box.position.x = 0.5
-box.rotation.y = 0.5
+box.position.x = 0.5;
+box.rotation.y = 0.5;
 
-camera.position.x = 5
-camera.position.y = 5
-camera.position.z = 5
+camera.position.x = 5;
+camera.position.y = 5;
+camera.position.z = 5;
 
-camera.lookAt(scene.position)
+camera.lookAt(scene.position);
 
 function animate(): void {
-	requestAnimationFrame(animate)
-	render()
+  requestAnimationFrame(animate);
+  render();
 }
 
 function render(): void {
-	let timer = 0.002 * Date.now()
-	box.position.y = 0.5 + 0.5 * Math.sin(timer)
-	box.rotation.x += 0.1
-	renderer.render(scene, camera)
+  const timer = 0.002 * Date.now();
+  box.position.y = 0.5 + 0.5 * Math.sin(timer);
+  box.rotation.x += 0.1;
+  renderer.render(scene, camera);
 }
 
-animate()
+animate();
--- webpack.config.js
+++ webpack.config.js
@@ -1,75 +1,53 @@
-/* Configure HTMLWebpack plugin */
-const HtmlWebpackPlugin = require('html-webpack-plugin')
-const HTMLWebpackPluginConfig = new HtmlWebpackPlugin({
-    template: __dirname + '/src/index.html',
-    filename: 'index.html',
-    inject: 'body'
-})
+const { resolve } = require('path');
 
-/* Configure BrowserSync */
-const BrowserSyncPlugin = require('browser-sync-webpack-plugin')
-const BrowserSyncPluginConfig = new BrowserSyncPlugin({
-    host: 'localhost',
-    port: 3000,
-    proxy: 'http://localhost:8080/'
-}, config = {
-    reload: false
-})
+const { CheckerPlugin } = require('awesome-typescript-loader');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const ProgressBarPlugin = require('progress-bar-webpack-plugin');
 
-/* Configure ProgressBar */
-const ProgressBarPlugin = require('progress-bar-webpack-plugin')
-const ProgressBarPluginConfig = new ProgressBarPlugin()
-
-/* Export configuration */
 module.exports = {
-    mode: 'development',
-    devtool: 'source-map',
-    entry: [
-        './src/index.ts'
+  mode: 'development',
+
+  entry: './src/index.ts',
+  output: {
+    filename: 'bundle.js',
+    path: resolve(__dirname, 'dist'),
+  },
+
+  resolve: {
+    extensions: ['.ts', '.tsx', '.js', '.jsx'],
+  },
+
+  devtool: 'source-map',
+
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        use: ['awesome-typescript-loader'],
+      },
+      {
+        test: /\.(jpe?g|png|gif|svg)$/i,
+        loaders: [
+          'file-loader?hash=sha512&digest=hex&name=images/[hash].[ext]',
+          'image-webpack-loader?bypassOnDebug&optipng.optimizationLevel=7&gifsicle.interlaced=false',
+        ],
+      },
+      {
+        test: /\.css$/i,
+        use: ['style-loader', 'css-loader'],
+      },
     ],
-    output: {
-        path: __dirname + '/dist',
-        filename: 'index.js'
-    },
-    module: {
-        rules: [
-            {
-                test: /\.ts$/,
-                use: 'awesome-typescript-loader'
-            }, {
-                test: /\.css$/,
-                exclude: /[\/\\]src[\/\\]/,
-                use: [
-                    {
-                        loader: 'style-loader',
-                        options: {
-                            sourceMap: true
-                        }
-                    },
-                    {loader: 'css-loader'}
-                ]
-            }, {
-                test: /\.css$/,
-                exclude: /[\/\\](node_modules|bower_components|public)[\/\\]/,
-                use: [
-                    {
-                        loader: 'style-loader',
-                        options: {
-                            sourceMap: true
-                        }
-                    },
-                    {
-                        loader: 'css-loader',
-                        options: {
-                            modules: true,
-                            importLoaders: 1,
-                            localIdentName: '[path]___[name]__[local]___[hash:base64:5]'
-                        }
-                    }
-                ]
-            }
-        ]
-    },
-    resolve: { extensions: [".web.ts", ".web.js", ".ts", ".js"] },
-    plugins: [HTMLWebpackPluginConfig, BrowserSyncPluginConfig, ProgressBarPluginConfig]
-}
+  },
+  resolve: {
+    extensions: ['.ts', '.tsx', '.js', '.jsx'],
+  },
+  plugins: [
+    new CheckerPlugin(),
+    new ProgressBarPlugin(),
+    new HtmlWebpackPlugin({
+      template: `${__dirname}/src/index.html`,
+      filename: 'index.html',
+      inject: 'body',
+    }),
+  ],
+};
